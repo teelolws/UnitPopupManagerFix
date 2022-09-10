8ecda795bb473868f1c2ae2132074e8dd5459724
Teelo <49792789+teelolws@users.noreply.github.com>
2022-09-10 23:42:42 +1200

    Update UPMFix.lua

--- UPMFix/UPMFix.lua
+++ UPMFix/UPMFix.lua
@@ -1,36 +1,36 @@
 function UnitPopupManager:OnUpdate(elapsed)
-	if ( not DropDownList1:IsShown() ) then
-		return;
-	end
+    if ( not DropDownList1:IsShown() ) then
+        return;
+    end
 
-	if ( not UnitPopup_HasVisibleMenu() ) then
-		return;
-	end
-	local count, wasLastSeparator;
-	for level, dropdownFrame in pairs(OPEN_DROPDOWNMENUS) do
-		if(dropdownFrame) then
+    if ( not UnitPopup_HasVisibleMenu() ) then
+        return;
+    end
+    local count, wasLastSeparator;
+    for level, dropdownFrame in pairs(OPEN_DROPDOWNMENUS) do
+        if(dropdownFrame) then
             count = 0
             if level == 1 then
                 count = 2
             end
-			local menu = self:GetMenu(dropdownFrame.which);
-			local topLevelButtons = menu:GetButtons(); 
-			local menuButtons; 
-			if(level == 2) then 
-				local nestedMenu = topLevelButtons[UIDROPDOWNMENU_MENU_VALUE];
-				local nestedMenusButtons = nestedMenu and nestedMenu:GetButtons(); 
-				menuButtons = nestedMenusButtons; 
+            local menu = self:GetMenu(dropdownFrame.which);
+            local topLevelButtons = menu:GetButtons(); 
+            local menuButtons; 
+            if(level == 2) then 
+                local nestedMenu = topLevelButtons[UIDROPDOWNMENU_MENU_VALUE];
+                local nestedMenusButtons = nestedMenu and nestedMenu:GetButtons();
+                menuButtons = nestedMenusButtons; 
             else
-				menuButtons =  topLevelButtons;
-			end 
-			if (menuButtons) then 
-				for index, button in ipairs(menuButtons) do 
-					local shown = button:CanShow(); 
-					if(shown) then
-						count = count + 1;
-						local enable = UnitPopupSharedUtil:IsEnabled(button); 
-						if(button.isSubsectionTitle) then 
-							if button:GetText() == UNIT_FRAME_DROPDOWN_SUBSECTION_TITLE_OTHER then
+                menuButtons =  topLevelButtons;
+            end
+            if (menuButtons) then
+                for index, button in ipairs(menuButtons) do
+                    local shown = button:CanShow();
+                    if(shown) then
+                        count = count + 1;
+                        local enable = UnitPopupSharedUtil:IsEnabled(button);
+                        if(button.isSubsectionTitle) then 
+                            if button:GetText() == UNIT_FRAME_DROPDOWN_SUBSECTION_TITLE_OTHER then
                                 count = count + 1
                             end
                         elseif button.isSubsection then
@@ -38,17 +38,17 @@ function UnitPopupManager:OnUpdate(elapsed)
                                 count = count - 1
                             end
                             wasLastSeparator = true
-						elseif (not button.isSubsection) then
+                        elseif (not button.isSubsection) then
                             wasLastSeparator = false
                             if (enable) then
-								UIDropDownMenu_EnableButton(level, count);
-							else 
-								UIDropDownMenu_DisableButton(level, count);
-							end
-						end		
-					end 
-				end
-			end 
-		end
-	end 
+                                UIDropDownMenu_EnableButton(level, count);
+                            else
+                                UIDropDownMenu_DisableButton(level, count);
+                            end
+                        end
+                    end 
+                end
+            end 
+        end
+    end 
 end 
