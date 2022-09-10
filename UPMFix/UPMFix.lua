77fbda067910686697d278d4dea26f820c66cccf
Teelo <49792789+teelolws@users.noreply.github.com>
2022-09-11 10:15:12 +1200

    Update UPMFix.lua

--- UPMFix/UPMFix.lua
+++ UPMFix/UPMFix.lua
@@ -1,57 +1,19 @@
-function UnitPopupManager:OnUpdate(elapsed)
-    if ( not DropDownList1:IsShown() ) then
-        return;
-    end
-
-    if ( not UnitPopup_HasVisibleMenu() ) then
-        return;
-    end
-    local count, wasLastSeparator;
-    for level, dropdownFrame in pairs(OPEN_DROPDOWNMENUS) do
-        if(dropdownFrame) then
-            count = 0
-            if level == 1 then
-                count = 2
-            end
-            local menu = self:GetMenu(dropdownFrame.which);
-            local topLevelButtons = menu:GetButtons(); 
-            local menuButtons; 
-            if(level == 2) then 
-                local nestedMenu = topLevelButtons[UIDROPDOWNMENU_MENU_VALUE];
-                local nestedMenusButtons = nestedMenu and nestedMenu:GetButtons();
-                menuButtons = nestedMenusButtons; 
-            else
-                menuButtons =  topLevelButtons;
-            end
-            if (menuButtons) then
-                for index, button in ipairs(menuButtons) do
-                    local shown = button:CanShow();
-                    if(shown) then
-                        if not ((count == 2) and (button:GetText() == RAID_TARGET_ICON)) then
-                            count = count + 1;
-                        end
-                        local enable = UnitPopupSharedUtil:IsEnabled(button);
-                        if(button.isSubsectionTitle) then 
-                            if (button:GetText() == UNIT_FRAME_DROPDOWN_SUBSECTION_TITLE_OTHER) or (button:GetText() == UNIT_FRAME_DROPDOWN_SUBSECTION_TITLE_LOOT) or (button:GetText() == UNIT_FRAME_DROPDOWN_SUBSECTION_TITLE_INSTANCE) or (button:GetText() == UNIT_FRAME_DROPDOWN_SUBSECTION_TITLE_INTERACT) then
-                                count = count + 1
-                            end
-                        elseif button.isSubsection then
-                            if wasLastSeparator then
-                                count = count - 1
-                            end
-                            wasLastSeparator = true
-                        elseif (not button.isSubsection) then
-                            wasLastSeparator = false
-                            if (enable) then
-                                UIDropDownMenu_EnableButton(level, count);
-                            else
-                                UIDropDownMenu_DisableButton(level, count);
-                            end
-                            --UIDropDownMenu_SetButtonText(level, count, tostring(_G["DropDownList"..level.."Button"..count]:GetText())..count..button:GetText())
-                        end
-                    end 
-                end
-            end 
-        end
-    end 
-end 
+function UnitPopupMenuCommunitiesGuildMember:GetMenuButtons()
+	return {
+		UnitPopupTargetButtonMixin,
+		UnitPopupAddFriendMenuButtonMixin,  
+		UnitPopupVoiceChatMicrophoneVolumeButtonMixin, 
+		UnitPopupVoiceChatSpeakerVolumeButtonMixin,
+		UnitPopupVoiceChatUserVolumeButtonMixin, 
+		UnitPopupInteractSubsectionTitle,
+		UnitPopupMenuFriendlyPlayerInviteOptions, --Submenu
+		UnitPopupWhisperButtonMixin,
+		UnitPopupIgnoreButtonMixin,
+		UnitPopupGuildPromoteButtonMixin,
+		UnitPopupOtherSubsectionTitle,
+		UnitPopupGuildLeaveButtonMixin,
+		UnitPopupReportClubMemberButtonMixin,
+		UnitPopupCopyCharacterNameButtonMixin,
+		UnitPopupCancelButtonMixin, 
+	}
+end
